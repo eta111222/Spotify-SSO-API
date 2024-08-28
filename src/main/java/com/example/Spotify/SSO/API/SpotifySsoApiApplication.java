3bcea1e58fff2653a54c41085680e453a545d7b5
eta111222 <146103162+eta111222@users.noreply.github.com>
2024-08-28 14:02:55 +0200

    initial commit

---
+++ src/main/java/com/example/Spotify/SSO/API/SpotifySsoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spotify.SSO.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifySsoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifySsoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spotify/SSO/API/SpotifySsoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spotify.SSO.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifySsoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
