f7dd2a7431496f3030eda417a9d9c0bc32aa117d
eta111222 <146103162+eta111222@users.noreply.github.com>
2024-08-28 15:37:07 +0200

    change project structure and delete three empty files,spotify,sso and api

--- src/main/java/com/example/SpotifySsoApiApplication.java
+++ src/main/java/com/example/SpotifySsoApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.Spotify.SSO.API;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
